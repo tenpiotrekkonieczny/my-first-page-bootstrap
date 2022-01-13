896c6a81a4791fa084f79472a77885986e03c576
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-01-13 11:34:59 +0100

    change-header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+const nav = document.querySelector('.navbar')
+
+function addShadow() {
+    if (window.scrollY >= 50) {
+        nav.classList.add('shadow-bg')
+    } else {
+        nav.classList.remove('shadow-bg')
+    }
+}
+
+
+window.addEventListener('scroll', addShadow)
+})
