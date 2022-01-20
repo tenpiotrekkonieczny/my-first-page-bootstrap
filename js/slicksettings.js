721c54b1a585bbfe62a777a24f539b3c4b62a9de
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-01-20 23:24:04 +0100

    end page

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -1,18 +1,18 @@
-$(".team-carousel").slick({
-	arrows: false,
-	autoplay: true,
-	pauseOnFocus: true,
-	mobileFirst: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: { slidesToShow: 2 },
-		},
-		{
-			breakpoint: 992,
-			settings: { slidesToShow: 3 },
-		},
-	],
+$('.team-carousel').slick({
+  arrows: false,
+  autoplay: true,
+  pauseOnFocus: true,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: { slidesToShow: 2 },
+    },
+    {
+      breakpoint: 992,
+      settings: { slidesToShow: 3 },
+    },
+  ],
 });
